d465e7c4fc24922a4c47e0fe823b65a214e4b2be
vagrant <vagrant@precise32.(none)>
2015-03-26 05:04:07 +0000

    Logging, layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.lograge.enabled = true
+  config.web_console.whitelisted_ips = '10.0.0.0/16'
 end
